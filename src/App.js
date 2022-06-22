f8524ad8d7d4138156cd924ea8ad1c7dbeb3110c
Marius <marius@example.com>
2022-06-22 11:39:16 +0300

    propTypes

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
         Small btn
       </Button>
       <Button>Click me</Button>
-      <Button variant='secondary'>Click me</Button>
-      <Button customFontSize='30'>customFontSize 30</Button>
+      <Button>ha</Button>
+      <Button customFontSize={30}>customFontSize 30</Button>
     </div>
   );
 }
