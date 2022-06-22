58bdd7d7665dc8010896686637966fe37482fdee
Marius <marius@example.com>
2022-06-22 11:03:27 +0300

    btn variants

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Storybook
 
+1. `npx sb init` inicijuojam storybook
+
+## button
+
 1. Button componentas
 2. `<Button variant="primary">` variant gali buti 'primary' "secondary" "danger"
+3. prop size kuri gali buti small, medium, large
+
+## Input
+
+1. `<Input color="light | dark">` input elementas kuris gali buti tamsus arba sviesus
+2. `<Input type="text | textarea">` jei input gauna textarea prop jis atvaizduoja textarea
+
+## Icon
+
+1.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className='App'>
       <h1>Heloo again world</h1>
+      <Button size='large'>Large btn</Button>
+      <Button variant='secondary' size='small'>
+        Small btn
+      </Button>
+      <Button>Click me</Button>
+      <Button variant='secondary'>Click me</Button>
+      <Button customFontSize='30'>customFontSize 30</Button>
     </div>
   );
 }
